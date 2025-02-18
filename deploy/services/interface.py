2d1e1bece508c26dd8c0cb8af274fd7021153a7f
hungnq131002 <hungnq131002@gmail.com>
2025-02-18 09:59:38 +0700

    chores: define again interface due to the mutual exclusive of two services

--- deploy/services/interface.py
+++ deploy/services/interface.py
@@ -5,18 +5,10 @@ from abc import ABC, abstractmethod
 class InterfaceService(ABC):
     '''Interface for Face Recognition Service'''
 
-    @abstractmethod
-    def embed_query(self, texts: List[Any], **kwargs) -> List[Any]:
-        ...
-
     @abstractmethod 
-    def embed_context(self, texts: List[Any], **kwargs) -> List[Any]:
+    def embed(self, texts: List[Any], **kwargs) -> List[Any]:
         ...
 
     @abstractmethod
-    def post_documents(self, texts: List[Any], **kwargs) -> List[Any]:
-        ...
-
-    @abstractmethod
-    def retrieve(self, query: List[Any], **kwargs) -> List[Any]:
+    def run(self, texts: List[Any], **kwargs) -> List[Any]:
         ...
