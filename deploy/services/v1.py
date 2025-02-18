902161d85eba5653aa9b442cd4b2037fe850fb94
hungnq131002 <hungnq131002@gmail.com>
2025-02-18 17:20:38 +0700

    chores: add more updated idea

--- deploy/services/v1.py
+++ deploy/services/v1.py
@@ -1,6 +1,6 @@
 
 from interface import InterfaceService
-from db.interface import QdrantFaceDatabase
+from db.interface import InterfaceDatabase
 
 from typing import List, Dict, Any
 from fastapi.responses import JSONResponse
@@ -13,26 +13,39 @@ class RetrievalServicesV1(InterfaceService):
         self,
         query_module: BaseModule,
         context_module: BaseModule, 
-        database_manager,
+        user_db: InterfaceDatabase,
+        doc_db: InterfaceDatabase,
+        chunk_db: InterfaceDatabase,
+        session_db: InterfaceDatabase
     ):
         super().__init__()
         self.query_module = query_module
         self.context_module = context_module
-        self.database_manager = database_manager
+        self.user_db = user_db
+        self.doc_db = doc_db
+        self.chunk_db = chunk_db
+        self.session_db = session_db
 
+    def register_user(self, user_name, authen):
+        ...
+    
     def check_user(self, token: str):
-        return self.database_manager.check_user(token)
+        ...
     
     def post_docs(self, docs: List[Any]):
         chunks = ...
         texts = [doc['text'] for doc in docs]
         payload = [doc['payload'] for doc in docs]
         embeds = self.context_module.embed(chunks)
-        self.database_manager.upload_docs(texts, embeds, payload)
+        ...
 
     def retrieve(self, texts: List[str], token: str) -> List[Dict[str, Any]]:
         if not self.check_user(token):
             return JSONResponse(content={"Error": "User not authenticated!"})
         query_embed = self.query_module.embed(texts)
-        retrieve_chunks = self.database_manager.search(query_embed)
+        retrieve_chunks = self.chunk_db.search(query_embed)
         return retrieve_chunks
+
+    def update_session_state(self, history: List[Any]):
+        ...
+    
