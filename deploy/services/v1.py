18547aadc1b6d7b3b675f47ecdba8378825a2317
hungnq131002 <hungnq131002@gmail.com>
2025-02-18 10:38:10 +0700

    chores: fix calling undefined attribute, remove comment, change name arguemnt, change output return

--- deploy/services/v1.py
+++ deploy/services/v1.py
@@ -9,11 +9,11 @@ from schema.module import BaseModule
 class PostServiceV1(InterfaceService):
     def __init__(
         self, 
-        ctx_module: BaseModule,
+        module: BaseModule,
         docs_db: QdrantFaceDatabase,
         chunk_db: QdrantFaceDatabase
     ):
-        self.ctx_module = ctx_module
+        self.ctx_module = module
         self.docs_db = docs_db
         self.chunk_db = chunk_db
 
@@ -26,16 +26,16 @@ class PostServiceV1(InterfaceService):
             return_tensors="np"
         )
         try:
-            outputs: Dict[Any] = self.model.run(data = [
+            outputs: Dict[Any] = self.ctx_module.model.run(data = [
                 text_responses['input_ids'], 
                 text_responses['attention_mask'], 
                 text_responses['token_type_ids']
             ])
             outputs = outputs.values()[0] # BxLx768
-            content = content.reshape(len(texts), -1, 768)[:, 0].tolist()
-            return JSONResponse(content=content)
+            content = content.reshape(len(texts), -1, 768)[:, 0].tolist() # Bx768
+            return content
         except Exception as e:
-            return JSONResponse(content={"Error": "Inference failed with error: " + str(e)})
+            return []
 
     def run(self, texts: List[str], **kwargs) -> List[List[float]]:
         chunking = ...
@@ -50,11 +50,11 @@ class PostServiceV1(InterfaceService):
 class RetrieveServiceV1(InterfaceService):
     def __init__(
         self, 
-        qry_module: BaseModule,
+        module: BaseModule,
         docs_db: QdrantFaceDatabase,
         chunk_db: QdrantFaceDatabase
     ):
-        self.qry_module = qry_module
+        self.qry_module = module
         self.docs_db = docs_db
         self.chunk_db = chunk_db
 
@@ -67,20 +67,16 @@ class RetrieveServiceV1(InterfaceService):
             return_tensors="np"
         )
         try:
-            outputs: Dict[Any] = self.model.run(data = [
+            outputs: Dict[Any] = self.qry_module.model.run(data = [
                 text_responses['input_ids'], 
                 text_responses['attention_mask'], 
                 text_responses['token_type_ids']
             ])
             outputs = outputs.values() # BxLx768
-            ## onnx parse
-            # content = outputs["embeddings"][:, 0].tolist()
-            ## trt parse
-            # content = outputs[:, 0].tolist() # Bx768
-            content = content.reshape(len(texts), -1, 768)[:, 0].tolist()
-            return JSONResponse(content=content)
+            content = content.reshape(len(texts), -1, 768)[:, 0].tolist() # Bx768
+            return content
         except Exception as e:
-            return JSONResponse(content={"Error": "Inference failed with error: " + str(e)})
+            return []
 
     def run(self, texts: List[str], **kwargs) -> List[List[float]]:
         embeds = self.embed(texts)
