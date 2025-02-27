9430d00aa92221f421644f72e3eeeeba069a24d7
tranchitam123 <tranchitam65a@gmail.com>
2025-02-27 13:20:39 +0700

    resolve conflict

--- deploy/api/app.py
+++ deploy/api/app.py
@@ -1,9 +1,16 @@
 import os
 from typing import List, Any, Optional, Union
 
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse 
+from fastapi import FastAPI, Depends, HTTPException, Form
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2AuthorizationCodeBearer
+import requests
+from pydantic import BaseModel
+
+from jwt import PyJWKClient
+import jwt
+from typing import Annotated
 
 from ray import serve
 from dotenv import load_dotenv
@@ -41,6 +48,21 @@ top_k = int(os.getenv("TOP_K", 5))
 threshold = float(os.getenv("THRESHOLD", 0.5))
 QDRANT_DB     = os.getenv("QDRANT_DB", "")
 
+# Keycloak Configuration
+KEYCLOAK_URL = os.getenv("KEYCLOAK_URL", "...")
+KEYCLOAK_REALM = os.getenv("KEYCLOAK_REALM", "...")
+KEYCLOAK_AUDIENCE = os.getenv("KEYCLOAK_AUDIENCE", "...")
+# KEYCLOAK_CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID", "...")
+# KEYCLOAK_CLIENT_SECRET = os.getenv("KEYCLOAK_CLIENT_SECRET", "...")
+# KEYCLOAK_ADMIN_USERNAME = os.getenv("KEYCLOAK_ADMIN_USERNAME", "...")
+# KEYCLOAK_ADMIN_PASSWORD = os.getenv("KEYCLOAK_ADMIN_PASSWORD", "...")
+
+# URLs
+TOKEN_URL = f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token"
+USER_URL = f"{KEYCLOAK_URL}/admin/realms/{KEYCLOAK_REALM}/users"
+JWKS_URL = f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/certs"
+ 
+
 print(f"Query model: {query_retriever_name}")
 print(f"Context model: {ctx_retriever_name}")
 print(f"URL: {url}")
@@ -55,11 +77,45 @@ print(f"QDRANT_DB: {QDRANT_DB}")
 # FastAPI Definition
 ############
 
-#?: flexible version
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl=f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token",
+    authorizationUrl=f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/auth",
+    refreshUrl=f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token",
+)
+
+# Hàm xác thực Access Token với Keycloak
+async def valid_access_token(access_token: Annotated[str, Depends(oauth_2_scheme)]):
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(JWKS_URL, headers=optional_custom_headers)
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience=KEYCLOAK_AUDIENCE,
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+# Model cho đăng ký & đăng nhập
+class UserRegister(BaseModel):
+    username: str
+    password: str
+    email: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
 app = FastAPI()
+
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:7777",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -152,11 +208,11 @@ class FastAPIDeployment:
         self.service_app = service_app
         # self.insert_app = insert_app
 
-    @app.get("/hello")
+    @app.get("/hello", dependencies=[Depends(oauth_2_scheme)])
     def hello(self, name: str) -> JSONResponse:
         return JSONResponse(content={"message": f"Hello, {name}!"})
 
-    @app.post("/retrieve_chunks")
+    @app.post("/retrieve_chunks", dependencies=[Depends(oauth_2_scheme)])
     async def retrieve_chunks(self, query: str, chunker_id: str) -> JSONResponse:
         # add remote with async func
         chunks = await self.service_app.retrieve_chunks.remote(query, chunker_id)
@@ -164,30 +220,29 @@ class FastAPIDeployment:
             return JSONResponse(content={"Error": "No chunks found!"})
         return JSONResponse(content=chunks)
     
-    @app.post("/insert_chunks")
+    @app.post("/insert_chunks", dependencies=[Depends(oauth_2_scheme)])
     async def insert_chunks(self, chunks: List[dict], chunker_id: str) -> JSONResponse:
         # add remote with async func
         response = await self.service_app.insert_chunks.remote(chunks, chunker_id)
         return JSONResponse(content=response)
     
-    @app.delete("/delete/{chunk_ids}/{chunker_id}")
+    @app.delete("/delete/{chunk_ids}/{chunker_id}", dependencies=[Depends(oauth_2_scheme)])
     async def delete_chunk_ids(self, chunk_ids: Union[str, List[str]], chunker_id: str) -> JSONResponse:
         # add remote with async func
         response = await self.service_app.delete_chunks.remote(chunk_ids=chunk_ids, chunker_id=chunker_id)
         return JSONResponse(content=response)
     
-    @app.delete("/delete/{doc_id}/{chunker_id}")
+    @app.delete("/delete/{doc_id}/{chunker_id}", dependencies=[Depends(oauth_2_scheme)])
     async def delete_doc_id(self, doc_id: str, chunker_id: str) -> JSONResponse:
         # add remote with async func
         response = await self.service_app.delete_chunks.remote(doc_id=doc_id, chunker_id=chunker_id)
         return JSONResponse(content=response)
     
-    @app.delete("/delete_chunker/{chunker_id}")
+    @app.delete("/delete_chunker/{chunker_id}", dependencies=[Depends(oauth_2_scheme)])
     async def delete_chunker_id(self, chunker_id: str) -> JSONResponse:
         # add remote with async func
         response = await self.service_app.delete_chunker.remote(chunker_id)
         return JSONResponse(content=response)
-
 
 # 2: Deploy the deployment.
 mainapp = FastAPIDeployment.bind(service_app1)
